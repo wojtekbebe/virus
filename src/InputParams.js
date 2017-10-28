b25f1c9273e829a1fe5c1bdb9d8f25aa35f93aac
Marzena <marzena.falkiewicz.kr@gmail.com>
2017-10-28 14:36:48 +0200

    added inputs

--- src/InputParams.js
+++ src/InputParams.js
@@ -24,8 +24,23 @@ export default class InputParams extends Component {
   render() {
     return (
       <div className="inputparams">
-        <input onChange={this.onChange1.bind(this)} />
-        <button onClick={this.onButtonClick.bind(this)} />
+        <div className="form-group">
+          <label for="infected">Procent zarażonych</label>
+          <input id="infected" className="form-control" onChange={this.onChange1.bind(this)} />
+        </div>
+        <div className="form-group">
+          <label for="immune">Procent odpornych</label>
+          <input id="immune" className="form-control" onChange={this.onChange1.bind(this)} />
+        </div>
+        <div className="form-group">
+          <label for="ratio1">Współczynnik zaraźliwości</label>
+          <input id="ratio1" className="form-control" onChange={this.onChange1.bind(this)} />
+        </div>
+        <div className="form-group">
+          <label for="ratio2">Współczynnik wyleczalnosci</label>
+          <input id="ratio2" className="form-control" onChange={this.onChange1.bind(this)} />
+        </div>
+        <button onClick={this.onButtonClick.bind(this)}>Rozpocznij symulację</button>
       </div>
     )
   }
