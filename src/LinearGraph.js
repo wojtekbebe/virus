3d561c1251a056ec5c286253e02dda7b4f7f2969
Marzena <marzena.falkiewicz.kr@gmail.com>
2017-10-29 11:25:18 +0100

    legend position

--- src/LinearGraph.js
+++ src/LinearGraph.js
@@ -21,13 +21,11 @@ export default class LinearGraph extends Component {
                     data={this.props.graphData}
                     stackOffset="expand"
                 >
-                    <Legend verticalAlign="bottom"/>
+                    <Legend wrapperStyle={{ bottom: -20, left: 20}} layout="horizontal" verticalAlign="bottom" align="middle"/>
                     <Area type="monotone" dataKey="Vp" stackId="1" name="w trakcie nabywania odporoności" dot={false} stroke="#222222" fill="#222222"/>
                     <Area type="monotone" dataKey="Ip" stackId="1" name="zakażeni" stroke="#FF0000" fill="#ff0000" dot={false}/>
                     <Area type="monotone" dataKey="Rp" stackId="1" name="odporni" stroke="#00FF00" fill="#00ff00" dot={false}/>
                     <Area type="monotone" dataKey="Sp" stackId="1" name="podatni" stroke="#FFA500" fill="#ffa500" dot={false}/>
-
-
 
                     <XAxis label={{ value:"liczba dni", position: "insideBottomRight", dy:10}} dataKey="t"/>
 
