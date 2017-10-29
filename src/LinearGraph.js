ccd425b3982c1df4c021d22074f87a605ee7050a
Wojciech Brygola <wojciech.brygola@gmail.com>
2017-10-29 11:18:38 +0100

    zmiana chartu

--- src/LinearGraph.js
+++ src/LinearGraph.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {LineChart, Legend, Line, XAxis, YAxis} from 'recharts';
+import {AreaChart, LineChart, Area, Legend, Line, XAxis, YAxis} from 'recharts';
 
 export default class LinearGraph extends Component {
 
@@ -15,19 +15,24 @@ export default class LinearGraph extends Component {
     render() {
         return (
             <div className="lineargraph" style={this.style}>
-                <LineChart
+                <AreaChart
                     width={500}
                     height={450}
                     data={this.props.graphData}
+                    stackOffset="expand"
                 >
                     <Legend verticalAlign="bottom"/>
-                    <Line type="monotone" dataKey="Sp" name="podatni" stroke="#FFA500" dot={false}/>
-                    <Line type="monotone" dataKey="Ip" name="zakażeni" stroke="#FF0000" dot={false}/>
-                    <Line type="monotone" dataKey="Rp" name="odporni" stroke="#00FF00" dot={false}/>
-                    <Line type="monotone" dataKey="Vp" name="w trakcie nabywania odporoności" dot={false} stroke="#222222"/>
+                    <Area type="monotone" dataKey="Vp" stackId="1" name="w trakcie nabywania odporoności" dot={false} stroke="#222222" fill="#222222"/>
+                    <Area type="monotone" dataKey="Ip" stackId="1" name="zakażeni" stroke="#FF0000" fill="#ff0000" dot={false}/>
+                    <Area type="monotone" dataKey="Rp" stackId="1" name="odporni" stroke="#00FF00" fill="#00ff00" dot={false}/>
+                    <Area type="monotone" dataKey="Sp" stackId="1" name="podatni" stroke="#FFA500" fill="#ffa500" dot={false}/>
+
+
+
                     <XAxis label={{ value:"liczba dni", position: "insideBottomRight", dy:10}} dataKey="t"/>
+
                     <YAxis label={{ value:"% populacji", position: "insideTopLeft", dy:-10, dx:60}}/>
-                </LineChart>
+                </AreaChart>
             </div>
         )
     }
