b9888b734b4eaf1a407c29a839faa1e49b75992e
Wojciech Brygola <wojciech.brygola@gmail.com>
2017-10-29 10:43:09 +0100

    colors

--- src/LinearGraph.js
+++ src/LinearGraph.js
@@ -21,9 +21,9 @@ export default class LinearGraph extends Component {
                     data={this.props.graphData}
                 >
                     <Legend verticalAlign="bottom"/>
-                    <Line type="monotone" dataKey="Sp" name="podatni" stroke="#00FF00" dot={false}/>
+                    <Line type="monotone" dataKey="Sp" name="podatni" stroke="#FFA500" dot={false}/>
                     <Line type="monotone" dataKey="Ip" name="zakażeni" stroke="#FF0000" dot={false}/>
-                    <Line type="monotone" dataKey="Rp" name="odporni" stroke="#0000FF" dot={false}/>
+                    <Line type="monotone" dataKey="Rp" name="odporni" stroke="#00FF00" dot={false}/>
                     <Line type="monotone" dataKey="Vp" name="w trakcie nabywania odporoności" dot={false} stroke="#222222"/>
                     <XAxis label={{ value:"liczba dni", position: "insideBottomRight", dy:10}} dataKey="t"/>
                     <YAxis label={{ value:"% populacji", position: "insideTopLeft", dy:-10, dx:60}}/>
